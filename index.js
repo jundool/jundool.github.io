5c1096d4c86ee8b1b9e2538f1c294e8b8ccf0557
jundool <152733115+jundool@users.noreply.github.com>
2023-12-21 16:55:24 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+/* 1. 새로고침(Refresh) 버튼 연결 */
+function refreshPage() {
+  location.reload();
+}
+
+
+
+
+
+/* nav를 이용한 동작하는 효과 메뉴_작동안함_지워도 상관없음 */
+let verticalBar = document.getElementById("vertical-underline");
+let horizontalBar = document.getElementById("horizontal-underline");
+let horizontalMenus = document.querySelectorAll("nav:first-child a");
+let verticalMenus = document.querySelectorAll("nav:nth-child(2) a");
+
+function verticalIndicator(e) {
+  verticalBar.style.left = e.offsetLeft + "px";
+  verticalBar.style.width = e.offsetWidth + "px";
+  verticalBar.style.top = e.offsetTop + e.offsetHeight + "px";
+}
+function horizontalIndicator(e) {
+  horizontalBar.style.left = e.offsetLeft + "px";
+  horizontalBar.style.width = e.offsetWidth + "px";
+  horizontalBar.style.top = e.offsetTop + e.offsetHeight + "px";
+}
+horizontalMenus.forEach((menu) =>
+  menu.addEventListener("click", (e) =>
+    horizontalIndicator(e.currentTarget)
+  )
+);
+verticalMenus.forEach((menu) =>
+  menu.addEventListener("click", (e) => verticalIndicator(e.currentTarget))
+);
+/* nav를 이용한 동작하는 효과 메뉴_작동안함 */
+
+
