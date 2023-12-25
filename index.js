11a0b16c0613692417486fe78f97ac5dd5a1f62c
jundool <152733115+jundool@users.noreply.github.com>
2023-12-25 21:01:11 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,33 +4,10 @@ function refreshPage() {
 }
 
 
-
-
-
-/* nav를 이용한 동작하는 효과 메뉴_작동안함_지워도 상관없음 */
-let verticalBar = document.getElementById("vertical-underline");
-let horizontalBar = document.getElementById("horizontal-underline");
-let horizontalMenus = document.querySelectorAll("nav:first-child a");
-let verticalMenus = document.querySelectorAll("nav:nth-child(2) a");
-
-function verticalIndicator(e) {
-  verticalBar.style.left = e.offsetLeft + "px";
-  verticalBar.style.width = e.offsetWidth + "px";
-  verticalBar.style.top = e.offsetTop + e.offsetHeight + "px";
+/* 2.  뒤로가기 버튼 */
+function goBack() {
+  window.history.back();
 }
-function horizontalIndicator(e) {
-  horizontalBar.style.left = e.offsetLeft + "px";
-  horizontalBar.style.width = e.offsetWidth + "px";
-  horizontalBar.style.top = e.offsetTop + e.offsetHeight + "px";
-}
-horizontalMenus.forEach((menu) =>
-  menu.addEventListener("click", (e) =>
-    horizontalIndicator(e.currentTarget)
-  )
-);
-verticalMenus.forEach((menu) =>
-  menu.addEventListener("click", (e) => verticalIndicator(e.currentTarget))
-);
-/* nav를 이용한 동작하는 효과 메뉴_작동안함 */
+
 
 
