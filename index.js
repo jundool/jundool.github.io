2a46638f7fc833d2ded13f25ceb0059027572256
jundool <152733115+jundool@users.noreply.github.com>
2024-01-05 02:47:18 +0900

    Update index.js

--- index.js
+++ index.js
@@ -10,4 +10,12 @@ function goBack() {
 }
 
 
+/* 3.  메뉴 토글버튼으로 메뉴 보이게 하기 */
+const tooglebutton = document.querySelector('.top_menu_tooglebutton');
+const top_menu = document.querySelector('.top_menu_1');
 
+tooglebutton.addEventListener('click', () => {
+  top_menu.classList.toggle('active');
+
+
+});
